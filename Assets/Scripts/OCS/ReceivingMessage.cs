4ca690184e573aeb28a852e74c726e8a3cb8a924
Ryana <ryana.karaki@etu.unice.fr>
2022-01-14 17:34:01 +0100

    First server test

---
+++ Assets/Scripts/OCS/ReceivingMessage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ReceivingMessage : MonoBehaviour
+{
+    public OSC osc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        osc.SetAllMessageHandler(OnReceiveX);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnReceiveX(OscMessage message)
+    {
+        string res = "";
+        foreach(object obj in message.values)
+        {
+            res += obj + " ";
+        }
+        Debug.Log(res);
+    }
+}
---
+++ Assets/Scripts/Server/Server.js
@@ -0,0 +1,19 @@
+﻿const WebSocket = require('ws');
+
+const server = new WebSocket.Server({port: 8080}, () => {
+    console.log("Server started")    
+});
+
+
+let sockets = [];
+server.on('connection', function connection(socket) {
+    sockets.push(socket);
+    socket.on('message', function (msg) {
+        console.log(msg.toString());
+        socket.send(msg.toString());
+    })
+    socket.on('close', function (){
+        sockets = sockets.filter(s => s !== socket);
+    });
+});
+
