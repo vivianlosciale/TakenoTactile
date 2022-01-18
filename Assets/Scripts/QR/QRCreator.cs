c30c6b262f4bbf2547b479f4e7eb5e8d0242412a
Martin Bouteiller <martin.bouteiller@etu.unice.fr>
2022-01-18 10:50:23 +0100

    QR code reader (phone) and builder (PC)

---
+++ Assets/Scripts/QR/QRCreator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRCreator : MonoBehaviour
+{
+    public string QRText;
+
+    void OnGUI()
+    {
+        Texture2D myQR = generateQR(QRText);
+        if (GUI.Button(new Rect(300, 300, 256, 256), myQR, GUIStyle.none)) { }
+    }
+
+    private static Color32[] Encode(string textForEncoding, int width, int height)
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+
+    public Texture2D generateQR(string text)
+    {
+        var encoded = new Texture2D(256, 256);
+        var color32 = Encode(text, encoded.width, encoded.height);
+        encoded.SetPixels32(color32);
+        encoded.Apply();
+        return encoded;
+    }
+}
