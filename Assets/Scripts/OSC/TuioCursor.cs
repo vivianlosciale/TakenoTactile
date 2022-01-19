f2f7baef0497614938e725732add57cdcefbd578
vivianlosciale <vivian.losciale@etu.univ-cotedazur.fr>
2022-01-19 19:18:14 +0100

    detect user input: click, drag and longclick

---
+++ Assets/Scripts/OSC/TuioCursor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class TuioCursor
+{
+    private float xCoord;
+    private float yCoord;
+    private int id;
+    private bool alreadyUpdate;
+    private CursorState previousState;
+    private CursorState state;
+    public int Id { get => id; set => id = value; }
+
+    public CursorState State
+    {
+        get => state;
+        set
+        {
+            previousState = state;
+            state = value;
+        }
+    }
+    public float XCoord { get => xCoord; }
+    public float YCoord { get => yCoord; }
+    public bool AlreadyUpdate { get => alreadyUpdate; }
+
+
+
+    public TuioCursor(int id, float x, float y)
+    {
+        this.id = id;
+        xCoord = x;
+        yCoord = y;
+        state = CursorState.CLICK_DOWN;
+        state = CursorState.CLICK_DOWN;
+        alreadyUpdate = false;
+    }
+
+    public Boolean isClick()
+    {
+        return previousState == CursorState.CLICK_DOWN && state == CursorState.CLICK_UP;
+    }
+
+    public Boolean isDrag()
+    {
+        return previousState == CursorState.DRAG && state == CursorState.CLICK_UP;
+    }
+
+    public Boolean isLongClick()
+    {
+        return previousState == CursorState.LONG_CLICK && state == CursorState.CLICK_UP;
+    }
+
+    public void updateCoordinates(float xCoord, float yCoord)
+    {
+        if (this.xCoord != xCoord || this.yCoord != yCoord)
+        {
+            alreadyUpdate = true;
+            previousState = state;
+            state = CursorState.DRAG;
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+        }
+    }
+}
---
+++ Assets/Scripts/OSC/TuioObject.cs
@@ -0,0 +1,6 @@
+﻿class TuioObject
+{
+    public TuioObject()
+    {
+    }
+}
