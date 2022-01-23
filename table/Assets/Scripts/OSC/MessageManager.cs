a993f19dabe086103a2050d58d5a648bb3366a77
vivianlosciale <vivian.losciale@etu.univ-cotedazur.fr>
2022-01-23 15:03:33 +0100

    reduce code

--- table/Assets/Scripts/OSC/MessageManager.cs
+++ table/Assets/Scripts/OSC/MessageManager.cs
@@ -10,33 +10,26 @@ public class MessageManager : MonoBehaviour
     public OSC osc;
     public TextMeshPro text;
 
-    List<TuioCursor> tuioCur = new List<TuioCursor>();
-    List<TuioObject> tuioObj = new List<TuioObject>();
-    List<TuioCursor> deadTouches = new List<TuioCursor>();
-    List<TuioObject> deadTouchesObj = new List<TuioObject>();
+    List<TuioEntity> tuioEvents = new List<TuioEntity>();
+    List<TuioEntity> deadTouches = new List<TuioEntity>();
+
+    private const string cursor = "/tuio/2Dcur";
+    private const string obj = "/tuio/2Dobj";
 
     // Start is called before the first frame update
     void Start()
     {
-        osc.SetAddressHandler("/tuio/2Dcur", generate2DMouseEvent);
-        osc.SetAddressHandler("/tuio/2Dobj", generate2DObjectEvent);
+        osc.SetAddressHandler(cursor, generate2DTUIOEvent);
+        osc.SetAddressHandler(obj, generate2DTUIOEvent);
     }
 
-    private void generate2DObjectEvent(OscMessage oscM)
+    private void generate2DTUIOEvent(OscMessage oscM)
     {
         string message = getMessage(oscM);
-        manageTuioObjectEvent(message);
+        manageTuioObjectEvent(message, oscM.address);
     }
 
-    //generate 3 line on clic down and update/drag, 5 lines for drag/update
-    private void generate2DMouseEvent(OscMessage oscM)
-    {
-        string message = getMessage(oscM);
-        manageTuioCursorEvent(message);
-
-    }
-
-    private void manageTuioObjectEvent(string message)
+    private void manageTuioObjectEvent(string message, string adress)
     {
         string[] messageTab = message.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
         List<string> tmp = new List<string>(messageTab);
@@ -44,50 +37,23 @@ public class MessageManager : MonoBehaviour
         {
             case "alive":
                 tmp.RemoveAt(0);
-                updateCollectionObj(tmp);
+                updateCollection(tmp, adress);
                 break;
             case "set":
                 tmp.RemoveAt(0);
-                checkObjectObj(tmp);
+                if (adress == obj)
+                    checkObjectObj(tmp);
+                else
+                    checkObject(tmp);
                 break;
             case "fseq":
                 string str = "Voici les detections:\n";
-                foreach (TuioObject t in tuioObj)
+                foreach (TuioEntity t in tuioEvents)
                 {
                     str = str + t;
                 }
                 text.SetText(str);
-                tuioObj = tuioObj.Except(deadTouchesObj).ToList();
-                break;
-        }
-    }
-
-    private void manageTuioCursorEvent(string message)
-    {
-        string[] messageTab = message.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
-        List<string> tmp = new List<string>(messageTab);
-        switch (tmp[0])
-        {
-            case "alive":
-                tmp.RemoveAt(0);
-                updateCollection(tmp);
-                break;
-            case "set":
-                tmp.RemoveAt(0);
-                checkObject(tmp);
-                break;
-            case "fseq":
-                string str = "Voici les detections:\n";
-                foreach (TuioCursor t in tuioCur)
-                {
-                    str = str + t;
-                    if (t.isClick())
-                    {
-                        //server.Broadcast("Hello World !");
-                    }
-                }
-                text.SetText(str);
-                tuioCur = tuioCur.Except(deadTouches).ToList();
+                tuioEvents = tuioEvents.Except(deadTouches).ToList();
                 break;
         }
     }
@@ -97,11 +63,11 @@ public class MessageManager : MonoBehaviour
         int id = int.Parse(tmp[0]);
         float xCoord = float.Parse(tmp[1]);
         float yCoord = float.Parse(tmp[2]);
-        TuioCursor tuioEvent = tuioCur.Find(e => e.Id == id);
+        TuioCursor tuioEvent = (TuioCursor)tuioEvents.Find(e => e.Id == id);
         if (tuioEvent == null)
         {
             tuioEvent = new TuioCursor(id, xCoord, yCoord);
-            tuioCur.Add(tuioEvent);
+            tuioEvents.Add(tuioEvent);
             StartCoroutine(instantiateType(tuioEvent));
         }
         else
@@ -119,12 +85,12 @@ public class MessageManager : MonoBehaviour
         float xCoord = float.Parse(tmp[2]);
         float yCoord = float.Parse(tmp[3]);
         float angle = float.Parse(tmp[4]);
-        TuioObject tuioEvent = tuioObj.Find(e => e.Id == id);
+        TuioObject tuioEvent = (TuioObject)tuioEvents.Find(e => e.Id == id);
         if (tuioEvent == null)
         {
             tuioEvent = new TuioObject(id, xCoord, yCoord, angle, value);
-            tuioObj.Add(tuioEvent);
-            StartCoroutine(instantiateTypeObj(tuioEvent));
+            tuioEvents.Add(tuioEvent);
+            StartCoroutine(instantiateType(tuioEvent));
         }
         else
         {
@@ -134,30 +100,21 @@ public class MessageManager : MonoBehaviour
         }
 
     }
-    private IEnumerator instantiateTypeObj(TuioObject tuioEvent)
+
+    private IEnumerator instantiateType(TuioEntity tuioEvent)
     {
         yield return new WaitForSeconds(1.0f);
-        if (tuioObj.Contains(tuioEvent) && tuioEvent.State == TuioState.CLICK_DOWN)
+        if (tuioEvents.Contains(tuioEvent) && tuioEvent.State == TuioState.CLICK_DOWN)
             tuioEvent.State = TuioState.LONG_CLICK;
     }
 
-    private IEnumerator instantiateType(TuioCursor tuioEvent)
+    private void updateCollection(List<string> idAlive, string adress)
     {
-        yield return new WaitForSeconds(1.0f);
-        if (tuioCur.Contains(tuioEvent) && tuioEvent.State == TuioState.CLICK_DOWN)
-            tuioEvent.State = TuioState.LONG_CLICK;
-    }
-
-    private void updateCollection(List<string> idAlive)
-    {
-        deadTouches = tuioCur.FindAll(e => (!idAlive.Contains(e.Id.ToString())));
-        foreach (TuioCursor t in deadTouches)
-            t.State = TuioState.CLICK_UP;
-    }
-    private void updateCollectionObj(List<string> idAlive)
-    {
-        deadTouchesObj = tuioObj.FindAll(e => (!idAlive.Contains(e.Id.ToString())));
-        foreach (TuioObject t in deadTouchesObj)
+        if (adress == obj)
+            deadTouches = tuioEvents.FindAll(e => !(idAlive.Contains(e.Id.ToString()) && !(e is TuioCursor)));
+        else
+            deadTouches = tuioEvents.FindAll(e => !idAlive.Contains(e.Id.ToString()) && !(e is TuioObject));
+        foreach (TuioEntity t in deadTouches)
             t.State = TuioState.CLICK_UP;
     }
 
