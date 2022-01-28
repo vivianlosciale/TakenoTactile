69e0abc63b861718a096ffd8b93061bd3cde108e
vivianlosciale <vivian.losciale@etu.univ-cotedazur.fr>
2022-01-28 15:03:55 +0100

    visual feedback when clicking on deck

--- table/Assets/Scripts/OSC/TestEvent.cs
+++ table/Assets/Scripts/OSC/TestEvent.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEvent : MonoBehaviour
 {
+
     // Start is called before the first frame update
     public void test()
     {
         Debug.Log("This is a onclick donw");
     }
-    public void test2()
+    public void pickCard(GameObject board)
     {
-        Debug.Log("This is a onclick up");
+        Transform pointPosition = board.transform.GetChild(0).transform;
+        StartCoroutine(TranslateCard(pointPosition));
+    }
+
+    private IEnumerator TranslateCard(Transform pointPosition)
+    {
+        int nbOfCard = 3;
+        GameObject prefab = Resources.Load("Models/Tiles") as GameObject;
+        for (int i = 0; i < nbOfCard; i++)
+        {
+            GameObject instance = Instantiate(prefab, transform.position, Quaternion.Euler(90, 0, 60));
+            instance.AddComponent<TileMovement>().setPosition(pointPosition.position);
+            yield return new WaitForSeconds(0.5f);
+        }
+
     }
 }
