19d34874ba95b05625082107885c0b53b0fdd842
Frederic M <frederic.metereau@etu.univ-cotedazur.fr>
2022-02-07 15:48:45 +0100

    Type for Card

---
+++ server/server/Utils/Game/CardType.cs
@@ -0,0 +1,32 @@
+namespace server.Utils.Game;
+
+public enum CardTypes
+{
+    None,
+    Panda,
+    Land,
+    Bamboo,
+}
+
+
+public static class CardTypesMethods
+{
+    public static string ToString(CardTypes cardType)
+    {
+        string? res = Enum.GetName(typeof(CardTypes), cardType);
+        if (res == null) return "None";
+        return res;
+    }
+
+    public static CardTypes ToPowers(string cardType)
+    {
+        try
+        {
+            return (CardTypes)Enum.Parse(typeof(CardTypes), cardType);
+        }
+        catch (Exception)
+        {
+            return CardTypes.None;
+        }
+    }
+}
