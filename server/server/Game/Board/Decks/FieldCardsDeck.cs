be3c941eef4b123888bba58a8acb6fe32b61dbb6
LudovicMarti <ludovic.marti@etu.unice.fr>
2022-02-09 18:59:05 +0100

    server: implements all field victory cards

--- server/server/Game/Board/Decks/FieldCardsDeck.cs
+++ server/server/Game/Board/Decks/FieldCardsDeck.cs
@@ -13,6 +13,90 @@ public class FieldCardsDeck: Deck<FieldVictoryCard>
         List<TileColor> colors = new List<TileColor>() 
             {TileColor.Green, TileColor.Yellow, TileColor.Yellow, TileColor.Green};
         Items.Add(new FieldVictoryCard("card_land_2g2yw", 3, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopRight,RelativePosition.Left,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Pink,TileColor.Green,TileColor.Green,TileColor.Pink};
+        Items.Add(new FieldVictoryCard("card_land_2r2gw", 4, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopRight,RelativePosition.Left,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Yellow,TileColor.Pink,TileColor.Pink,TileColor.Yellow};
+        Items.Add(new FieldVictoryCard("card_land_2r2yw", 5, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Green,TileColor.Green,TileColor.Green};
+        Items.Add(new FieldVictoryCard("card_land_3gw1", 2, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.TopLeft};
+        colors = new List<TileColor>()
+            {TileColor.Green,TileColor.Green,TileColor.Green};
+        Items.Add(new FieldVictoryCard("card_land_3gw2", 2, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.BottomLeft};
+        colors = new List<TileColor>()
+            {TileColor.Green,TileColor.Green,TileColor.Green};
+        Items.Add(new FieldVictoryCard("card_land_3gw3", 2, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Pink,TileColor.Pink,TileColor.Pink};
+        Items.Add(new FieldVictoryCard("card_land_3rw1", 4, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.TopLeft};
+        colors = new List<TileColor>()
+            {TileColor.Pink,TileColor.Pink,TileColor.Pink};
+        Items.Add(new FieldVictoryCard("card_land_3rw2", 4, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.BottomLeft};
+        colors = new List<TileColor>()
+            {TileColor.Pink,TileColor.Pink,TileColor.Pink};
+        Items.Add(new FieldVictoryCard("card_land_3rw3", 4, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Yellow,TileColor.Yellow,TileColor.Yellow};
+        Items.Add(new FieldVictoryCard("card_land_3yw1", 3, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.TopLeft};
+        colors = new List<TileColor>()
+            {TileColor.Yellow,TileColor.Yellow,TileColor.Yellow};
+        Items.Add(new FieldVictoryCard("card_land_3yw2", 3, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopLeft,RelativePosition.BottomLeft};
+        colors = new List<TileColor>()
+            {TileColor.Yellow,TileColor.Yellow,TileColor.Yellow};
+        Items.Add(new FieldVictoryCard("card_land_3yw3", 3, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopRight,RelativePosition.Left,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Green,TileColor.Green,TileColor.Green,TileColor.Green};
+        Items.Add(new FieldVictoryCard("card_land_4gw", 3, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopRight,RelativePosition.Left,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Pink,TileColor.Pink,TileColor.Pink,TileColor.Pink};
+        Items.Add(new FieldVictoryCard("card_land_4rw", 5, BuildCondition(positions, colors)));
+
+        positions = new List<RelativePosition>()
+            {RelativePosition.TopRight,RelativePosition.Left,RelativePosition.TopRight};
+        colors = new List<TileColor>()
+            {TileColor.Yellow,TileColor.Yellow,TileColor.Yellow,TileColor.Yellow};
+        Items.Add(new FieldVictoryCard("card_land_4yw", 4, BuildCondition(positions, colors)));
         
         Shuffle();
     }
--- server/server/Game/Board/Decks/TileDeck.cs
+++ server/server/Game/Board/Decks/TileDeck.cs
@@ -11,11 +11,13 @@ public class TileDeck: Deck<Tile>
         Items.Add(new Tile("tiles_g3", TileColor.Green));
         Items.Add(new Tile("tiles_g4", TileColor.Green));
         Items.Add(new Tile("tiles_g5", TileColor.Green));
+        
         Items.Add(new Tile("tiles_y1", TileColor.Yellow));
         Items.Add(new Tile("tiles_y2", TileColor.Yellow));
         Items.Add(new Tile("tiles_y3", TileColor.Yellow));
         Items.Add(new Tile("tiles_y4", TileColor.Yellow));
         Items.Add(new Tile("tiles_y5", TileColor.Yellow));
+        
         Items.Add(new Tile("tiles_r1", TileColor.Pink));
         Items.Add(new Tile("tiles_r2", TileColor.Pink));
         Items.Add(new Tile("tiles_r3", TileColor.Pink));
