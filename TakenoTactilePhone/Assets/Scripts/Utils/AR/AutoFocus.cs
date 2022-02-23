cf01e829deb826435ae1d0c1ce22600b88db275c
Martin Bouteiller <martin.bouteiller@etu.unice.fr>
2022-02-23 11:23:23 +0100

    Display bamboo number and fix autofocus

--- TakenoTactilePhone/Assets/Scripts/Utils/AR/AutoFocus.cs
+++ TakenoTactilePhone/Assets/Scripts/Utils/AR/AutoFocus.cs
@@ -8,23 +8,11 @@ public class AutoFocus : MonoBehaviour
     void Start()
     {
         VuforiaApplication.Instance.OnVuforiaStarted += OnVuforiaStarted;
-        VuforiaApplication.Instance.OnVuforiaPaused += OnPaused;
     }
 
     private void OnVuforiaStarted()
     {
         VuforiaBehaviour.Instance.CameraDevice.SetFocusMode(
             FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-        VuforiaBehaviour.Instance.CameraDevice.SetCameraMode(Vuforia.CameraMode.MODE_DEFAULT);
-    }
-
-    private void OnPaused(bool paused)
-    {
-        if (!paused) // Resumed
-        {
-            // Set again autofocus mode when app is resumed
-            VuforiaBehaviour.Instance.CameraDevice.SetFocusMode(
-                FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-        }
     }
 }
