9a786600d6b0325a5d362d1032936047a38f2150
LudovicMarti <ludovic.marti@etu.unice.fr>
2022-03-01 17:58:10 +0100

    server: fix questionmark

--- server/server/Game/GameActions/DiceActions/ChoiceAction.cs
+++ server/server/Game/GameActions/DiceActions/ChoiceAction.cs
@@ -1,5 +1,7 @@
 using server.Game.Board;
 using server.SocketRooms;
+using server.Utils.Game;
+using server.Utils.Protocol;
 
 namespace server.Game.GameActions.DiceActions;
 
@@ -7,6 +9,10 @@ public class ChoiceAction: DiceAction
 {
     public override void Use(PlayerRoom player, TableRoom table, GameState game)
     {
-        // TODO
+        player.SendEvent(MessageQuery.WaitingChoseWeather);
+        DiceFaces diceFace = player.WaitingDiceFace();
+        Console.WriteLine("Player "+player.GetNumber()+" chose '"+DiceFacesMethods.ToString(diceFace)+"'");
+        table.SendEvent(MessageQuery.RollDice, diceFace.ToString());
+        GetPower(diceFace).Use(player,table,game);
     }
 }
