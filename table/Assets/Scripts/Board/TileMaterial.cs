75420a3336350c222ba9bb0283b8c4f79a819736
vivianlosciale <vivian.losciale@etu.univ-cotedazur.fr>
2022-03-02 11:06:13 +0100

    added twinkle tile

--- table/Assets/Scripts/Board/TileMaterial.cs
+++ table/Assets/Scripts/Board/TileMaterial.cs
@@ -11,10 +11,21 @@ public class TileMaterial : MonoBehaviour
     {
         meshRenderer = transform.GetComponent<MeshRenderer>();
         _value = meshRenderer.material.color.r;
-        StartCoroutine(DeactivateTile());
     }
 
-    private IEnumerator DeactivateTile()
+    public void DeactivateTile()
+    {
+
+        StartCoroutine(DeactivateTileAnimation());
+    }
+
+    public void TwinkleTile()
+    {
+
+        StartCoroutine(TwinkleTileAnimation());
+    }
+
+    private IEnumerator DeactivateTileAnimation()
     {
         Color c = meshRenderer.material.color;
 
@@ -24,6 +35,27 @@ public class TileMaterial : MonoBehaviour
             c.r = _value;
             c.g = _value;
             c.b = _value;
+            meshRenderer.materials[1].SetColor("_Color", c);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private IEnumerator TwinkleTileAnimation()
+    {
+        Color c = meshRenderer.material.color;
+        bool down = true;
+        while (true)
+        {
+            if(down)
+                _value -= _speed;
+            else
+                _value += _speed;
+
+            c.r = _value;
+            c.g = 0;
+            c.b = 0;
+            if (c.r < 0.0f || c.r > 1.0f)
+                down = !down;
             meshRenderer.materials[1].SetColor("_Color", c);
             yield return new WaitForSeconds(0.01f);
         }
