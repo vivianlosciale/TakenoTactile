ba2df9345644fe889e30448360ed75c6494bc53f
vivianlosciale <vivian.losciale@etu.univ-cotedazur.fr>
2022-03-04 08:51:57 +0100

    change collider tile

--- table/Assets/Scripts/Events/BoardTileMovement.cs
+++ table/Assets/Scripts/Events/BoardTileMovement.cs
@@ -5,12 +5,18 @@ class BoardTileMovement : MonoBehaviour
 {
     Vector3 pointPosition;
     private readonly float speed = 9.5f;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<MeshCollider>().enabled = false;
+    }
     private void FixedUpdate()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, pointPosition, step);
         if(Vector3.Distance(transform.position, pointPosition) < 0.1f)
         {
+            gameObject.GetComponent<MeshCollider>().enabled = true;
             Destroy(this);
         }
     }
